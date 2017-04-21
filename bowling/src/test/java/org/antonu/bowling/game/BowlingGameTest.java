c92c7a7e6a781ca2de050212cc383941bce173b9
antonu17 <antonu17@gmail.com>
2017-04-21 16:37:19 +0600

    Add more unit tests

--- bowling/src/test/java/org/antonu/bowling/game/BowlingGameTest.java
+++ bowling/src/test/java/org/antonu/bowling/game/BowlingGameTest.java
@@ -55,7 +55,7 @@ public class BowlingGameTest {
         game.toss(6);
         game.toss(4);
         game.toss(2);
-        assertEquals(12, game.getCommittedRounds().get(0).getScore());
+        assertEquals(12, game.getTotalScore());
     }
 
     @Test
@@ -82,5 +82,59 @@ public class BowlingGameTest {
         game.toss(2);
         int score = game.getTotalScore();
         assertEquals(106, score);
+    }
+
+    @Test
+    public void testRoundSwitch() throws Exception {
+        Round round1 = game.getCurrentRound();
+        game.toss(4);
+        game.toss(5);
+        Round round2 = game.getCurrentRound();
+        assertNotEquals(round1, round2);
+    }
+
+    @Test
+    public void testGameFinish() throws Exception {
+        for (int i = 0; i < 12; i++) {
+            game.toss(10);
+        }
+        assertTrue(game.isFinished());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativePins() throws Exception {
+        game.toss(-5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPinsOverflow() throws Exception {
+        game.toss(7);
+        game.toss(7);
+    }
+
+    @Test
+    public void testSpareRound() throws Exception {
+        Round round = game.getCurrentRound();
+        game.toss(7);
+        game.toss(3);
+        assertTrue(round.isSpare());
+    }
+
+    @Test
+    public void testStrikeRound() throws Exception {
+        Round round = game.getCurrentRound();
+        game.toss(10);
+        assertTrue(round.isStrike());
+    }
+
+    @Test
+    public void testRound() throws Exception {
+        Round round = game.getCurrentRound();
+        round.toss(5);
+        assertFalse(round.isFinished());
+        round.toss(1);
+        assertFalse(round.isStrike());
+        assertFalse(round.isSpare());
+        assertTrue(round.isFinished());
     }
 }
