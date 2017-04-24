8f58f9848f8589ea1e54ee9273431cc65b63e466
antonu17 <antonu17@gmail.com>
2017-04-24 16:56:34 +0600

    Rewrite console-ui with spring-boot

--- console-ui/src/main/java/org/antonu/bowling/Launcher.java
+++ console-ui/src/main/java/org/antonu/bowling/Launcher.java
@@ -1,12 +1,17 @@
 package org.antonu.bowling;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Created by kit on 4/20/17.
  */
-public class Launcher {
 
+@SpringBootApplication
+@ComponentScan(basePackages = "org.antonu")
+public class Launcher {
     public static void main(String[] args) {
-        ConsoleBowlingGame game = new ConsoleBowlingGame();
-        game.play();
+        SpringApplication.run(Launcher.class, args);
     }
 }
