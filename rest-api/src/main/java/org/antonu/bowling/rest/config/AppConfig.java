c83a647e88da5eaa9b481bb6d263191b4565734a
antonu17 <antonu17@gmail.com>
2017-04-25 16:40:04 +0600

    REST API application configuration

--- rest-api/src/main/java/org/antonu/bowling/rest/config/AppConfig.java
+++ rest-api/src/main/java/org/antonu/bowling/rest/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.antonu.bowling.game.BowlingGame;
 import org.antonu.bowling.game.BowlingGameImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
 
 /**
  * Created by kit on 4/24/17.
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
+    @SessionScope
     public BowlingGame bowlingGame() {
         return new BowlingGameImpl();
     }
---
+++ rest-api/src/main/java/org/antonu/bowling/rest/config/Constants.java
@@ -0,0 +1,9 @@
+package org.antonu.bowling.rest.config;
+
+/**
+ * Created by kit on 4/25/17.
+ */
+public class Constants {
+
+    public static final String ENDPOINT_URL = "/game";
+}
