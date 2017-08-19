8abb2813278bd4ef4f1cacc490382dcf6704e119
antonu17 <antonu17@gmail.com>
2017-08-19 14:03:46 +0600

    Fix build-rc outputs

--- ci/build-artifact.sh
+++ ci/build-artifact.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-set -e -x
+set -e -u -x
 
 export ORG_GRADLE_PROJECT_version=$(cat version/version)
+
 cd bowling-game
 ./gradlew --no-daemon assemble
+cd ..
+
+cp bowling-game/console-ui/build/distributions/console-ui-*.zip build-artifact
