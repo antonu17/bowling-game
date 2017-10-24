0f9f348c5dd1d0549ada4f3a9de09b46e00c04f6
Anton Ustyuzhanin <au@hellofresh.com>
2017-10-24 15:56:17 +0200

    Modify build-artifact task

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export VERSION=$(cat ../version/${VERSION_FILE})
+export VERSION=$(cat ../version/version})
 sed -e "s/^\(version=\).*$/\1${VERSION}/" -i gradle.properties
 
 ./gradlew assemble
+
+mv console-ui/build/distributions/console-ui-*.* ../artifacts/
