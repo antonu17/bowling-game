821689ff1083cac1e5ab3f33706f0b12e4bd039d
Anton Ustyuzhanin <au@hellofresh.com>
2017-10-24 17:17:25 +0200

    Modify release message

--- ci/scripts/create-draft.sh
+++ ci/scripts/create-draft.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-TAG="draft.$(git --git-dir=$GIT_DIR rev-parse HEAD)"
+TAG="$(git --git-dir=$GIT_DIR rev-parse HEAD)"
 PR_ID=$(git --git-dir=$GIT_DIR config --get pullrequest.id)
 PR_AUTHOR=$(git --git-dir=$GIT_DIR log -n 1 --pretty=format:'%an')
 
 echo $TAG > $OUTPUT/tag
-echo -e "PR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > $OUTPUT/body
+echo -e "PR: #${PR_ID}, by ${PR_AUTHOR}\nLast commit id: \`${TAG}\`" > $OUTPUT/body
