c07564a6df1f6b75ac7b3c813e77b19c688afbd9
Anton Ustyuzhanin <au@hellofresh.com>
2017-10-24 16:55:36 +0200

    Fixes

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export VERSION=$(cat ../version/version})
+export VERSION=$(cat ../version/version)
 sed -e "s/^\(version=\).*$/\1${VERSION}/" -i gradle.properties
 
 ./gradlew assemble
 
-mv console-ui/build/distributions/console-ui-*.* ../artifacts/
+mv ./console-ui/build/distributions/console-ui-*.* ../artifacts/
