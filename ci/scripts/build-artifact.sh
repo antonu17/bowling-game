4c711b74ca8cf0d306b14cb77c2a9b73ed35bc1d
Anton Ustyuzhanin <au@hellofresh.com>
2017-10-24 17:51:19 +0200

    Compress tar artifact

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -4,5 +4,6 @@ export VERSION=$(cat ../version/version)
 sed -e "s/^\(version=\).*$/\1${VERSION}/" -i gradle.properties
 
 ./gradlew assemble
+bzip2 ./console-ui/build/distributions/console-ui-*.tar
 
 mv ./console-ui/build/distributions/console-ui-*.* ../artifacts/
